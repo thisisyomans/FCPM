2b8b40a347532db4d39a6056dfa8a26a5254546e
thisisyomans <tanejamm@gmail.com>
2019-01-04 15:03:01 -0800

    meh

--- testingarea/skarea1/examplewithdata.py
+++ testingarea/skarea1/examplewithdata.py
@@ -18,28 +18,28 @@ X = np.asarray(data["Breast_Can"])
 y = np.asarray(data["All_Cancer"])
 predictionData = [[100, 200, 300, 400, 500, 600]]
 predictionData1 = [[100]]
-predictionData2 = [[200]]
-predictionData3 = [[300]]
-predictionData4 = [[400]]
-predictionData5 = [[500]]
-predictionData6 = [[600]]
+#predictionData2 = [[200]]
+#predictionData3 = [[300]]
+#predictionData4 = [[400]]
+#predictionData5 = [[500]]
+#predictionData6 = [[600]]
 
 for model in classifiers:
     print(model)
     clf = model
     clf.fit(X.reshape(-1, 1), y)
     result1 = clf.predict(predictionData1)
-    result2 = clf.predict(predictionData2)
-    result3 = clf.predict(predictionData3)
-    result4 = clf.predict(predictionData4)
-    result5 = clf.predict(predictionData5)
-    result6 = clf.predict(predictionData6)
-    print(result1, '\n')
-    print(result2, '\n')
-    print(result3, '\n')
-    print(result4, '\n')
-    print(result5, '\n')
-    print(result6, '\n\n')
+    #result2 = clf.predict(predictionData2)
+    #result3 = clf.predict(predictionData3)
+    #result4 = clf.predict(predictionData4)
+    #result5 = clf.predict(predictionData5)
+    #result6 = clf.predict(predictionData6)
+    #print(result1, '\n')
+    #print(result2, '\n')
+    #print(result3, '\n')
+    #print(result4, '\n')
+    #print(result5, '\n')
+    print(result1, '\n\n')
 
 #Result reporting
 #1 = [2581.9454382], model = svm.SVR(gamma = 'scale')
